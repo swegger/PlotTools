1db468d4b13bb7c3783108b9a266a526d21b6d48
Seth Egger <swegger@mit.edu>
2017-06-08 10:32:23 -0400

    Added plotboxpos; swtiched to Evan's version of MarginalSubplots

--- MarginalSubplots.m
+++ MarginalSubplots.m
@@ -21,35 +21,42 @@ xHeight = Parser.Results.xHeight;
 yMargin = Parser.Results.yMargin;
 yHeight = Parser.Results.yHeight;
 
-%% Open figure
-ssz = get(0,'screensize');
-aspectRatio = ssz(3)/ssz(4);
-fh = figure('Units','normalized','Position',[0.1 0.1 0.3 0.3*aspectRatio]);
+hFig = figure('Position',[680   141   361   901]);
+hFig.SizeChangedFcn = @(a,b)StupidResize(a,b,xMargin,xHeight,yMargin,yHeight);
 
 %% Generate subplots
-    h{1} = subplot(1,3,1);
-    h{1}.PlotBoxAspectRatioMode = 'manual';
-    h{1}.PlotBoxAspectRatio = [1 1 1];
-    h{1}.DataAspectRatioMode = 'manual';
-    h{2} = subplot(1,3,2);
-    h{2}.PlotBoxAspectRatioMode = 'manual';
-    h{2}.PlotBoxAspectRatio = [1 yHeight 1];
-    h{2}.DataAspectRatioMode = 'manual';
-    h{3} = subplot(1,3,3);
-    h{3}.PlotBoxAspectRatioMode = 'manual';
-    h{3}.PlotBoxAspectRatio = [xHeight 1 1];
-    h{3}.DataAspectRatioMode = 'manual';
+h(1) = subplot(1,3,1);
+h(1).PlotBoxAspectRatioMode = 'manual';
+h(1).PlotBoxAspectRatio = [1 1 1];
+h(1).DataAspectRatioMode = 'manual';
+h(2) = subplot(1,3,2);
+h(2).PlotBoxAspectRatioMode = 'manual';
+h(2).PlotBoxAspectRatio = [1 yHeight 1];
+h(2).DataAspectRatioMode = 'manual';
+h(3) = subplot(1,3,3);
+h(3).PlotBoxAspectRatioMode = 'manual';
+h(3).PlotBoxAspectRatio = [xHeight 1 1];
+h(3).DataAspectRatioMode = 'manual';
 
 %% Adjust location of main axis
-h{1}.Position = [0.1 0.4 0.4 0.4];
-MainPos = h{1}.Position;
+h(1).Position = [0.1 0.4 0.4 0.4];
+MainPos = plotboxpos(h(1));
 
 %% Adjust locations of marginal axes
-h{2}.Position = [MainPos(1) MainPos(2)-yMargin-yHeight MainPos(3) yHeight];
-h{3}.Position = [MainPos(1)+xMargin+MainPos(3) MainPos(2) xHeight MainPos(4)];
+h(2).Position = [MainPos(1) MainPos(2)-yMargin-yHeight MainPos(3) yHeight];
+h(3).Position = [MainPos(1)+xMargin+MainPos(3) MainPos(2) xHeight MainPos(4)];
 
-h{3}.XDir = 'reverse';
-h{3}.YAxisLocation = 'right';
+h(3).XDir = 'reverse';
+h(3).YAxisLocation = 'right';
 
-disp('test')
+end
 
+function [] = StupidResize(hObject,event,xMargin,xHeight,yMargin,yHeight)
+
+h = hObject.Children;
+MainPos = plotboxpos(h(3));
+%% Adjust locations of marginal axes
+h(2).Position = [MainPos(1) MainPos(2)-yMargin-yHeight MainPos(3) yHeight];
+h(1).Position = [MainPos(1)+xMargin+MainPos(3) MainPos(2) xHeight MainPos(4)];
+
+end
